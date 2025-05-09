4c0252ba37db9425aa12f4f046cde04d5b9e799d
Paul Nnaemeka <emekapaul302@gmail.com>
2025-05-09 17:37:43 +0100

    Add env secret key

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,18 +1,24 @@
-import { createApp } from './appbuild.js';
-import mongoose from 'mongoose';
+import { createApp } from "./appbuild.js";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
 
+dotenv.config();
 
 const PORT = process.env.PORT || 3003;
-const HOST = process.env.HOST || 'localhost';
-const DBName = process.env.DBName || 'Scheduly';
+const HOST = process.env.HOST || "localhost";
 
 mongoose
-	.connect(`mongodb://${HOST}/${DBName}`)
-	.then(() => console.log("Connected to Database"))
-	.catch((err) => console.log(`Error: ${err}`));
+  .connect(process.env.MONGO_URI)
+  .then(() => {
+    console.log("✅ MongoDB Connected");
+  })
+  .catch((error) => {
+    console.error("❌ MongoDB Connection Failed:", error);
+    process.exit(1);
+  });
 
 const app = createApp();
 
 app.listen(PORT, HOST, () => {
-  console.log(`Server is running on http://${HOST}:${PORT}`)
-})
+  console.log(`Server is running on http://${HOST}:${PORT}`);
+});
