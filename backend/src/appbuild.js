8a3e833ef5987e5fdf6caa7406a228081b9d9684
Paul Nnaemeka <emekapaul302@gmail.com>
2025-05-19 04:16:04 +0100

    Use mongoDb for session storage

--- backend/src/appbuild.js
+++ backend/src/appbuild.js
@@ -1,32 +1,41 @@
 import express from "express";
-import passport from 'passport';
-import session from 'express-session';
-import './AuthStrategy/local-strategy.js';
-import router from './routes/main.js';
-
-
+import passport from "passport";
+import session from "express-session";
+import MongoStore from "connect-mongo";
+import "./AuthStrategy/local-strategy.js";
+import router from "./routes/main.js";
 
 export function createApp() {
   const app = express();
 
   app.use(express.json());
-  app.use(session({
-    secret: process.env.SESSIONSECRET || "scheduly",
-		saveUninitialized: true,
-		resave: false,
-		cookie: {
-			maxAge: 60000 * 60,
-		}
-  }))
-  //app.use(express.bodyParser);
+  app.use(
+    session({
+      secret: process.env.SESSIONSECRET || "scheduly",
+      saveUninitialized: false,
+      resave: false,
+      store: MongoStore.create({
+        mongoUrl: process.env.MONGODB_URI,
+        ttl: 14 * 24 * 60 * 60, // = 14 days
+        autoRemove: "native",
+      }),
+      cookie: {
+        maxAge: 14 * 24 * 60 * 60 * 1000, // = 14 days
+        secure: process.env.NODE_ENV === "production",
+        sameSite: "lax",
+      },
+    })
+  );
 
   app.use(passport.initialize());
   app.use(passport.session());
-  app.use(express.urlencoded({
-    extended: true
-  }));
+  app.use(
+    express.urlencoded({
+      extended: true,
+    })
+  );
 
   app.use(router);
 
-  return app
+  return app;
 }
