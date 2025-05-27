b83516bdedcb52f0d23cd7a9187d0287c1e05393
Paul Nnaemeka <emekapaul302@gmail.com>
2025-05-27 02:28:38 +0100

    Log error msg for debugging

--- frontend/src/utils/axios.js
+++ frontend/src/utils/axios.js
@@ -14,4 +14,42 @@ const axiosInstance = axios.create({
   },
 });
 
+// Add request interceptor for debugging
+axiosInstance.interceptors.request.use(
+  (config) => {
+    // Log the full URL and credentials setting
+    console.log("Making request to:", `${config.baseURL}${config.url}`);
+    console.log("With credentials:", config.withCredentials);
+    return config;
+  },
+  (error) => {
+    console.error("Request error:", error);
+    return Promise.reject(error);
+  }
+);
+
+// Add response interceptor for debugging
+axiosInstance.interceptors.response.use(
+  (response) => {
+    console.log("Response received:", {
+      status: response.status,
+      headers: response.headers,
+      cookies: document.cookie,
+    });
+    return response;
+  },
+  (error) => {
+    if (error.response) {
+      console.error("Response error:", {
+        status: error.response.status,
+        data: error.response.data,
+        headers: error.response.headers,
+      });
+    } else {
+      console.error("Network error:", error.message);
+    }
+    return Promise.reject(error);
+  }
+);
+
 export default axiosInstance;
