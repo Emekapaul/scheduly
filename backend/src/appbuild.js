b64f38e539a9bed7fd13f10455c07ffd45bd9cc7
Paul Nnaemeka <emekapaul302@gmail.com>
2025-05-27 02:02:21 +0100

    Add more methods to cors options

--- backend/src/appbuild.js
+++ backend/src/appbuild.js
@@ -16,6 +16,7 @@ export function createApp() {
           ? process.env.FRONTEND_URL
           : "http://localhost:3000",
       credentials: true,
+      methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
       allowedHeaders: [
         "Content-Type",
         "Authorization",
